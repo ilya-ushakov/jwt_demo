0902ba7b2065fcd16827330b7f3f147583713600
ILya <iluaushakov79@gmail.com>
2017-04-27 22:26:18 +0200

    spring security  angular and jwt

---
+++ src/main/java/com/jooq/proto/jwtangspr/security/transfer/JwtUserDto.java
@@ -0,0 +1,48 @@
+package com.jooq.proto.jwtangspr.security.transfer;
+
+/**
+ * Simple placeholder for info extracted from the JWT
+ *
+ * @author pascal alma
+ */
+public class JwtUserDto {
+
+    private Long id;
+
+    private String username;
+
+    private String role;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "JwtUserDto{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", role='" + role + '\'' +
+                '}';
+    }
+}
